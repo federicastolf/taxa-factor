bf8a753e17ae524a7865f10d759221bada911de7
federicastolf <federica.stolf@gmail.com>
2024-03-06 11:57:18 -0500

    .

--- fit_taxtree.R
+++ fit_taxtree.R
@@ -4,23 +4,42 @@ library(Rcpp)
 library(RcppArmadillo)
 library(tidyverse)
 
-load("data_try/tree_fungi.Rdata")
-load("data_try/fungi_binary.Rdata")
+# load("data_try/tree_fungi.Rdata")
+# load("data_try/fungi_binary.Rdata")
+load("/Users/stolffederica/Library/CloudStorage/Dropbox/dark taxa/code/fungi_data/allData_clim_trait.Rdata")
 source("function_fungi.R")
 source("Laplace_taxa.R")
 sourceCpp("LapTaxonomic.cpp")
 
-tree_fungi = tree_fungi[,2:7]
-nunk = nrow(tree_fungi[tree_fungi$Species=="unk",])
-lab = rep(0,nunk)
-for(i in 1:length(lab)){
-  lab[i] = paste0("unk",i) 
+# tree_fungi = tree_fungi[,2:7]
+taxonomy = taxonomy[,4:9]
+# nunk = nrow(tree_fungi[tree_fungi$Species=="unk",])
+# lab = rep(0,nunk)
+# for(i in 1:length(lab)){
+#   lab[i] = paste0("unk",i) 
+# }
+# idxunk = which(tree_fungi$Species=="unk")
+# tree_fungi$Species[idxunk] = lab
+
+#--# drop the columns that have only one child structure from phylum to species #--#
+phylan = as.character(taxonomy$phylum)
+phyla_cut = rep(0,155)
+j=1
+for(i in 1:length(phylan)){
+  t1 = taxonomy %>% filter(phylum==phylan[i])
+  if(nrow(t1)==1){
+    phyla_cut[j] = phylan[i]
+    j=j+1
+  }
 }
-idxunk = which(tree_fungi$Species=="unk")
-tree_fungi$Species[idxunk] = lab
+idxcut = which(taxonomy$phylum %in% phyla_cut)
+fungi = otu.table[,-idxcut]
+fungi = matrix(as.numeric(fungi >0), nrow(fungi), ncol(fungi))
+taxonomy = taxonomy[-idxcut,]
 
 param = list(max_it = 100, epsilon = 0.0001, burnin=500, Niter=2000, 
              eps_MH = 0.03, a_gamma=10, b_gamma=1/4)
 # param = list(max_it = 100, epsilon = 0.0001)
 
+fit_taxonomic = Ltaxa1(fungi, tree_fungi, param)
 fit_taxonomic = Ltaxa(fungi, tree_fungi, param)
--- function_fungi.R
+++ function_fungi.R
@@ -12,3 +12,23 @@ data_tree = function(Y, label){
   data_taxa= as.data.frame(sapply(data_taxa, as.numeric))
   data_taxa
 }
+
+# same as above, but drop only child
+dad_tree = function(Y, taxonomy, l){
+  taxonomy = taxonomy[,(l-1):l]
+  colnames(taxonomy) = c("vl1","vl")
+  data_all = cbind.data.frame(t(Y), "taxa" = taxonomy[,2])
+  data_taxa = data_all %>% 
+    group_by(taxa) %>%
+    summarise(across(where(is.numeric), max))
+  ddrop = taxonomy %>% 
+    group_by(vl1) %>%
+    summarise(ct = vl[which(length(unique(vl))==1)])
+  data_taxa = data_taxa[!(data_taxa$taxa %in% ddrop$ct),]
+  data_taxa = as.data.frame(t(data_taxa))
+  colnames(data_taxa) = data_taxa[1,]
+  data_taxa = data_taxa[-1,]
+  data_taxa= as.data.frame(sapply(data_taxa, as.numeric))
+  data_taxa
+}
+
